0be9fb913ea26aebca12b43f4ffc59a63fce1eed
Ray Cao <zisong.cw@alibaba-inc.com>
2025-03-31 11:10:20 +0800

    use rdb to startAppendonly after primary-replica full sync
    Signed-off-by: Ray Cao <zisong.cw@alibaba-inc.com>

--- src/server.h
+++ src/server.h
@@ -2984,6 +2984,7 @@ int rewriteAppendOnlyFileBackground(void);
 int loadAppendOnlyFiles(aofManifest *am);
 void stopAppendOnly(void);
 int startAppendOnly(void);
+int tryRestartAOFAfterSYNCWithRdb(void);
 void backgroundRewriteDoneHandler(int exitcode, int bysignal);
 void killAppendOnlyChild(void);
 void restartAOFAfterSYNC(void);
