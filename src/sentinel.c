5617b76c8be68dabe6f920a1a7dba1bc5db439aa
Josh Soref <2119212+jsoref@users.noreply.github.com>
2025-06-06 11:36:04 -0400

    spelling: timedout
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- src/sentinel.c
+++ src/sentinel.c
@@ -920,7 +920,7 @@ void sentinelCollectTerminatedScripts(void) {
 
 /* Kill scripts in timeout, they'll be collected by the
  * sentinelCollectTerminatedScripts() function. */
-void sentinelKillTimedoutScripts(void) {
+void sentinelKillTimedOutScripts(void) {
     listNode *ln;
     listIter li;
     mstime_t now = mstime();
@@ -5423,7 +5423,7 @@ void sentinelTimer(void) {
     sentinelHandleDictOfValkeyInstances(sentinel.primaries);
     sentinelRunPendingScripts();
     sentinelCollectTerminatedScripts();
-    sentinelKillTimedoutScripts();
+    sentinelKillTimedOutScripts();
 
     /* We continuously change the frequency of the server "timer interrupt"
      * in order to desynchronize every Sentinel from every other.
