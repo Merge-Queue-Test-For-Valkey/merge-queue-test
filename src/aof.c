9677a6c06a367fe82fd2a25694489c317819d76e
Venkat Pamulapati <pamuvenk@amazon.com>
2025-09-12 22:37:55 +0000

    Refactor return types on rdbLoadRioWithLoadingContext
    Signed-off-by: Venkat Pamulapati <pamuvenk@amazon.com>

--- src/aof.c
+++ src/aof.c
@@ -1452,7 +1452,7 @@ int loadSingleAppendOnlyFile(char *filename) {
 
         if (fseek(fp, 0, SEEK_SET) == -1) goto readerr;
         rioInitWithFile(&rdb, fp);
-        if (rdbLoadRio(&rdb, RDBFLAGS_AOF_PREAMBLE, NULL) != C_OK) {
+        if (rdbLoadRio(&rdb, RDBFLAGS_AOF_PREAMBLE, NULL) != RDB_OK) {
             if (old_style)
                 serverLog(LL_WARNING, "Error reading the RDB preamble of the AOF file %s, AOF loading aborted",
                           filename);
